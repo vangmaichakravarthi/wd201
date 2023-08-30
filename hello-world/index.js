cb3bc0a9f22983f4787495519fdaa11bd726b786
vangmaichakravarthi <134768240+vangmaichakravarthi@users.noreply.github.com>
2023-08-30 19:34:10 +0530

    Update index.js

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,44 @@
-const hello =() =>{
-    console.log("Hello Github!");
-};
-hello();
+/*const hello = () => {
+    console.log("Hello GitHub!");
+  };
+  
+  hello();
+*/
+
+//write new file using fs
+
+/*const fs = require("fs");
+
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+*/
+const fs = require("fs");
+//read file
+/*fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+*/
+//append file
+/*fs.appendFile("sample.txt", " This is my updated content", (err) => {
+  if (err) throw err;
+  console.log("File updated!");
+});
+*/
+//rename file
+/*fs.rename("sample.txt", "test.txt", (err) => {
+  if (err) throw err;
+  console.log("File name updated!");
+});
+*/
+//unlink file i.e delete files
+fs.unlink("test.txt", (err) => {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});
